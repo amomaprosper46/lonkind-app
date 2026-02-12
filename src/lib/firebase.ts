9a2d8314e9bd125cb448b7b6dd1c416dfaf2e499
amomaprosper46 <amomaprosper2525@gmail.com>
2026-02-11 17:22:32 -0700

    Add Firebase configuration validation function
    Added a function to validate Firebase configuration.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -18,23 +18,18 @@ const firebaseConfig = {
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
+export const isFirebaseConfigValid = 
+    !!firebaseConfig.apiKey &&
+    !!firebaseConfig.authDomain &&
+    !!firebaseConfig.projectId &&
+    !!firebaseConfig.storageBucket &&
+    !!firebaseConfig.messagingSenderId &&
+    !!firebaseConfig.appId;
+
+
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const storage = getStorage(app);
 export const rtdb = getDatabase(app);
-
-
-
-// Copy these key-value pairs into your Vercel project's Environment Variables settings:
-/*
-NEXT_PUBLIC_FIREBASE_API_KEY=AIzaSyAk4DcDNed4OWSahdV56ll1wI973-0wgS4
-NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN=impactful-ideas.firebaseapp.com
-NEXT_PUBLIC_FIREBASE_DATABASE_URL=https://impactful-ideas-default-rtdb.firebaseio.com
-NEXT_PUBLIC_FIREBASE_PROJECT_ID=impactful-ideas
-NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET=impactful-ideas.appspot.com
-NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID=494901200454
-NEXT_PUBLIC_FIREBASE_APP_ID=1:494901200454:web:0ea71cc5dbe22b22f6ac47
-NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID=G-P5TSD6JL5Y
-*/
